95ca8d8f97d9dcaeb9dc3b518bc70960ab1e6680
Pavel Alexandru <alex.pavel2002@gmail.com>
2021-12-04 14:24:16 +0200

    Adaugare cod oponenti in proiect si fisierele lor de input, output si input_recuperat

---
+++ decrypt_oponenti.py
@@ -0,0 +1,18 @@
+import sys
+
+fil = sys.argv[0]
+cheie = sys.argv[1]
+inp = sys.argv[2]
+ou = sys.argv[3]
+
+f = open(ou, "w")
+g = open(inp, "rb")
+
+i = 0
+sir_intrare = g.read(1)
+while sir_intrare:
+    f.write(chr(sir_intrare[0] ^ ord(cheie[i % len(cheie)])))
+    sir_intrare = g.read(1)
+
+g.close()
+f.close()
